244e75d034b4c99f812277a2a5489001f48e7624
furkandeveloper <furkan.dvlp@gmail.com>
2020-02-27 23:10:27 +0300

    Add Models for Mars Rover

---
+++ MarsRover/Models/Moves.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Models
+{
+    public class Moves
+    {
+        public string Direction { get; set; }
+
+        public int Number { get; set; }
+    }
+}
---
+++ MarsRover/Models/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static MarsRover.Models.Enums;
+
+namespace MarsRover.Models
+{
+    public class Position
+    {
+        public Position()
+        {
+            X = 1;
+            Y = 2;
+            Directory = Direction.N;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public Direction Directory { get; set; }
+    }
+}
