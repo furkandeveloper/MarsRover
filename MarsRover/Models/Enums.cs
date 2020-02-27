770324aa8e92527b28e7f1f9e40d89937dafa83c
furkandeveloper <furkan.dvlp@gmail.com>
2020-02-27 23:11:46 +0300

    Add enum for Direction and Move

---
+++ MarsRover/Models/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Models
+{
+    public class Enums
+    {
+        public enum Direction
+        {
+             N = 0,
+             S = 1,
+             E = 2,
+             W = 3
+        }
+
+        public enum Move
+        {
+            L = 0,
+            R = 1,
+            M = 2
+        }
+    }
+}
